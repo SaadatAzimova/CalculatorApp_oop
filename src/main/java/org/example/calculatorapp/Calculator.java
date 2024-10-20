8f12fc1bfbc59ef1aef8f1f759a5f78f049ed0cd
SaadatAzimova <saadatazimova5@gmail.com>
2024-10-20 16:27:02 +0600

    add commets

--- src/main/java/org/example/calculatorapp/Calculator.java
+++ src/main/java/org/example/calculatorapp/Calculator.java
@@ -1,23 +1,27 @@
 package org.example.calculatorapp;
-
 public class Calculator {
-    private double operand1;   // Stores the first operand
-    private double operand2;   // Stores the second operand
-    private char operator;     // Stores the arithmetic operator
-    private  double result;     // Stores the result of the operation
-    private boolean isError;   // Tracks if an error occurred (e.g., division by zero)
-
-    public boolean isError()                   // Returns true if an error occurred (e.g., division by zero)
+    private double operand1;// Stores the first operand
+    private double operand2;// Stores the second operand
+    private char operator;// Stores the arithmetic operator
+    private  double result;// Stores the result of the operation
+    public boolean isError()// Returns true if an error occurred (division by zero)
     {
-        return false;
+        if(operator=='/' && operand2==0){
+            return true;
+        }
+        else {return false;}
+    }
+    public void reset()// Resets all fields (for a new calculation)
+    {
+        operand1 = 0;
+        operand2 = 0;
+        operator = '\0';// No operator
+        result = 0;
+        boolean isError = false;
     }
 
-    public void reset()                        // Resets all fields (for a new calculation)
-    {
+    public void calculate()/* Performs the calculation based on the operator*/{
 
-    }
-
-    public void calculate(){
         if(this.operator == '+'){
             this.result = operand1 + operand2;
         }
@@ -25,34 +29,34 @@ public class Calculator {
         }
         else if(this.operator == '*'){this.result = operand1 * operand2;
         }
-        else if(this.operator == '/' && operand2!=0 ){this.result = operand1 / operand2;
+        else if(this.operator == '/' && operand2!=0 && !isError()){this.result = operand1 / operand2;
         }
-    };                    // Performs the calculation based on the operator
+    }
     public void setOperator(char operator) {
         this.operator = operator;
     }
-    public char getOperator() {
-        return operator;
-    }
+    //setter methods
     public void setOperand2(double operand2) {
         this.operand2 = operand2;
-    }
-
-    public double getOperand1() {
-        return operand1;
     }
     public void setOperand1(double operand1) {
         this.operand1 = operand1;
     }
-
-    public double getOperand2() {
-        return operand2;
-    }
-
-    public double getResult() {
-        return result;
-    }
     public void setResult(int result) {
         this.result = result;
     }
+    //getter methods
+    public double getOperand1() {
+        return operand1;
+    }
+    public double getOperand2() {
+        return operand2;
+    }
+    public char getOperator() {
+        return operator;
+    }
+    public double getResult() {
+        return result;
+    }
+
 }
--- src/main/java/org/example/calculatorapp/CalculatorController.java
+++ src/main/java/org/example/calculatorapp/CalculatorController.java
@@ -4,67 +4,67 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class CalculatorController {
-    @FXML
+        @FXML// The Calculator instance representing the model (logic handler)
         Calculator calc = new Calculator();
-        @FXML
+        @FXML // The TextField for displaying numbers and results in the UI
         private TextField text;
 
-        @FXML
+        @FXML // When button '1' is clicked, this method appends '1' to the current text in the TextField
         private void onClick1 () {
             String val = text.getText();
             val = val + "1";
             text.setText(val);
         }
-        @FXML
+        @FXML//// When button '2' is clicked, this method appends '2' to the current text in the TextField
         private void onClick2 () {
             String val = text.getText();
             val = val + "2";
             text.setText(val);
 
         }
-        @FXML
+        @FXML // When button '2' is clicked, this method appends '2' to the current text in the TextField
         private void onClick3 () {
             String val = text.getText();
             val = val + "3";
             text.setText(val);
         }
-        @FXML
+        @FXML// When button '4' is clicked, this method appends '4' to the current text in the TextField
         private void onClick4 () {
             String val = text.getText();
             val = val + "4";
             text.setText(val);
         }
-        @FXML
+        @FXML // When button '5' is clicked, this method appends '5' to the current text in the TextField
         private void onClick5 () {
             String val = text.getText();
             val = val + "5";
             text.setText(val);
         }
-        @FXML
+        @FXML // When button '6' is clicked, this method appends '6' to the current text in the TextField
         private void onClick6 () {
             String val = text.getText();
             val = val + "6";
             text.setText(val);
         }
-        @FXML
+        @FXML// When button '7' is clicked, this method appends '7' to the current text in the TextField
         private void onClick7() {
             String val = text.getText();
             val = val + "7";
             text.setText(val);
         }
-        @FXML
+        @FXML // When button '8' is clicked, this method appends '8' to the current text in the TextField
         private void onClick8 () {
             String val = text.getText();
             val = val + "8";
             text.setText(val);
         }
-        @FXML
+        @FXML// When button '9' is clicked, this method appends '9' to the current text in the TextField
         private void onClick9 () {
             String val = text.getText();
             val = val + "9";
             text.setText(val);
         }
-        @FXML
+        @FXML // When button '0' is clicked, this method appends '0' to the current text in the TextField
         private void onClick0 () {
             String val = text.getText();
             val = val + "0";
@@ -77,39 +77,43 @@ public class CalculatorController {
             calc.setOperand1(Integer.parseInt(operand1));
             calc.setOperator('+');
         }
-        @FXML
+        @FXML // When the '+' operator is clicked, the current value is set as operand1, and operator is set to '+'
         private void onMinusClick () {
             String operand1 = text.getText();
             text.setText("");
             calc.setOperand1(Integer.parseInt(operand1));
             calc.setOperator('-');
         }
-        @FXML
+        @FXML //operator is set to '*'
         private void onMultiplicationClick () {
             String operand1 = text.getText();
             text.setText("");
             calc.setOperand1(Integer.parseInt(operand1));
             calc.setOperator('*');
         }
-        @FXML
+        @FXML//operator is set to '/'
         private void onDivisionClick () {
             String operand1 = text.getText();
             text.setText("");
             calc.setOperand1(Integer.parseInt(operand1));
             calc.setOperator('/');
     }
-        @FXML
+        @FXML // When the '=' button is clicked, it performs the calculation and displays the result
         private void onEqualClick () {
-
-            String op2 = text.getText();
-            calc.setOperand2(Integer.parseInt(op2));
-            calc.calculate();
-            text.setText("" + calc.getResult());
-
+            if (calc.isError()){//isError is true when operator:'=', opperand2=0(we cant divide a digit into 0)
+                text.setText("not possible to calculate");
+            } else {
+                // Otherwise, display the valid result
+                String op2 = text.getText();
+                calc.setOperand2(Integer.parseInt(op2));
+                calc.calculate();
+                text.setText("" + calc.getResult());
+            }
         }
-        @FXML
-        private void onClearClick () {
+        @FXML//resets all fields and clears the textField
+        private void onClearClick (){
             text.setText("");
+            calc.reset();
         }
 
 
