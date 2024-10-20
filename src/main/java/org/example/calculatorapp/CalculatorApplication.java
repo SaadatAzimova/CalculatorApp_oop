79ec5f03e30adfe188b19792485145a1066f9145
SaadatAzimova <saadatazimova5@gmail.com>
2024-10-20 16:49:04 +0600

    remane view.fxml

--- src/main/java/org/example/calculatorapp/CalculatorApplication.java
+++ src/main/java/org/example/calculatorapp/CalculatorApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Saadat's calculator");
         stage.setScene(scene);
         stage.show();
     }
